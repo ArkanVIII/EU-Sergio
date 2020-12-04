3e859fe918db282aadb9a15a97dabba6a62619c7
Sergio <sergvives@gmail.com>
2020-12-04 20:45:20 +0100

    B3 creado B12 eliminado

--- Boletines/B3/Boletin3/src/boletin3/Boletin3.java
+++ Boletines/B3/Boletin3/src/boletin3/Boletin3.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package boletin12;
+package boletin3;
 
 /**
  *
  * @author sergv
  */
-public class Boletin12 {
+public class Boletin3 {
 
     /**
      * @param args the command line arguments
